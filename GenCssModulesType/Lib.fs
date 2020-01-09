47e5f4e531409021e15862f4acb8e083e74c7699
klcantrell <cantrellkalalau@gmail.com>
2020-01-08 20:44:53 -0500

    Transform rules into typedefs and save file

--- GenCssModulesType/Lib.fs
+++ GenCssModulesType/Lib.fs
@@ -1,5 +1,7 @@
 ﻿module Lib
 
+open System
+open System.IO
 open System.Text.RegularExpressions
 
 let parseRules str =
@@ -7,11 +9,45 @@ let parseRules str =
     let matches = Regex.Matches(str, rulePattern)
     if matches.Count > 0 then [for m in matches -> m.Value] else []
 
-let testInput = "
-    .sup
-        color: blue;
-    }
-    .dude {
-        color: red;
-    }
-"
+let printWithColor (color: ConsoleColor) (message: string) =
+    Console.ForegroundColor <- color
+    Console.WriteLine(message)
+    Console.ResetColor()
+
+let printErrorWithColor (color: ConsoleColor) (message: string) =
+    Console.ForegroundColor <- color
+    Console.Error.WriteLine(message)
+    Console.ResetColor()
+
+let removeDot (rule: string) =
+    rule.Substring(1)
+
+let typeDefOfRule rule =
+    "export const " + rule + ": string;"
+
+let toTypeDefFile content typeDef =
+    content + typeDef + "\n"
+
+let saveFile path content =
+   printWithColor ConsoleColor.Magenta ("Created/Updated " + path)
+   File.WriteAllText(path, content)
+
+let handleOnChange path =
+    let rec handler retries =
+        if retries > 10_000
+        then
+             "Could not open file" |> printErrorWithColor ConsoleColor.DarkRed 
+        else
+            try
+                File.ReadAllText path
+                |> parseRules
+                |> List.map removeDot
+                |> List.map typeDefOfRule
+                |> List.reduce toTypeDefFile
+                |> saveFile (path + ".d.ts")
+            with
+                | :? System.IO.IOException -> 
+                    ("Retrying, attempt: " + (retries + 1).ToString()) |> printWithColor ConsoleColor.Yellow
+                    handler (retries + 1)
+    handler 0
+
--- GenCssModulesType/Program.fs
+++ GenCssModulesType/Program.fs
@@ -2,35 +2,13 @@
 open System.IO
 open Lib
 
-let printWithColor (color: ConsoleColor) (message: string) =
-    Console.ForegroundColor <- color
-    Console.WriteLine(message)
-    Console.ResetColor()
-
-let printErrorWithColor (color: ConsoleColor) (message: string) =
-    Console.ForegroundColor <- color
-    Console.Error.WriteLine(message)
-    Console.ResetColor()
-
-let handleOnChange path =
-    let rec handler retries =
-        if retries > 10_000
-        then
-             "Could not open file" |> printErrorWithColor ConsoleColor.DarkRed 
-        else
-            try
-                File.ReadAllText path
-                |> parseRules
-                |> List.iter(
-                    fun rule -> ("The rule is " + rule) |> printWithColor ConsoleColor.Magenta)
-            with
-                | :? System.IO.IOException -> 
-                    ("Retrying, attempt: " + (retries + 1).ToString()) |> printWithColor ConsoleColor.Yellow
-                    handler (retries + 1)
-    handler 0
-
 [<EntryPoint>]
 let main argv =
+    printWithColor ConsoleColor.White "Edit a CSS file in this directory or subdirectories..."
+    printWithColor ConsoleColor.White "TypeScript type defs shall be generated"
+    Console.WriteLine()
+    printWithColor ConsoleColor.White "Enter q to quit"
+
     let fileSystemWatcher = new FileSystemWatcher()
     fileSystemWatcher.Path <- "."
     fileSystemWatcher.Filter <- "*.css"
